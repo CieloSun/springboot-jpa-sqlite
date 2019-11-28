2bf6d2d4aaa0fca3075f5bbecda1139db8acca58
CieloSun <632898354@qq.com>
2019-11-28 20:02:11 +0800

    fix static error

--- src/main/java/com/cielo/sqlite/demo/controller/TestController.java
+++ src/main/java/com/cielo/sqlite/demo/controller/TestController.java
@@ -6,17 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.SQLException;
-
 @RestController
 public class TestController {
     @Autowired
     private SQLiteService sqLiteService;
-
-    @GetMapping("test")
-    public String test() throws SQLException {
-        return sqLiteService.test();
-    }
 
     @GetMapping("jpa")
     public User testJPA(){
